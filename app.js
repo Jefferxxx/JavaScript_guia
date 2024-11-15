a56cd5b3b8c4e36e4fc545e6522c8720af9b9065
jeffersoncadavid <jeffersoncadavid2006@gmail.com>
2024-11-15 11:24:59 -0500

    trabajo de logica de programacion avances

---
+++ app.js
@@ -0,0 +1,60 @@
+var pato = "victor";
+let gato = "chino";
+
+// constantes
+const grillo = "lopez";
+
+
+// string
+
+// Definición de cadenas de texto
+// let saludo = "¡Hola, Jefferson!";
+// let pregunta = '¿Cómo estás?';
+
+// Concatenación de cadenas
+// let mensaje = saludo + ' ' + pregunta; Unir dos cadenas con el operador '+'
+// console.log(mensaje);   Salida: "¡Hola, Jefferson! ¿Cómo estás?"
+
+
+
+
+// template string
+
+// Definición de variables
+// let nombre = "Jefferson";
+// let edad = 30;
+
+// Interpolación de variables
+// let mensaje = `Hola, mi nombre es ${nombre} y tengo ${edad} años.`;
+// console.log(mensaje);  // "Hola, mi nombre es Jefferson y tengo 18 años."
+
+// Cadenas multilínea
+// let mensajeMultilinea = `Este es un ejemplo
+// de una cadena de texto
+// que ocupa varias líneas.`;
+// console.log(mensajeMultilinea);
+
+
+
+
+// numbers
+
+// Números enteros
+// let entero = 10;
+// console.log(entero);  
+
+// Números decimales
+// let decimal = 3.14;
+// console.log(decimal);  // 3.14
+
+// Operaciones matemáticas
+// let suma = 5 + 3;
+// let producto = 4 * 2;
+// let division = 10 / 2;
+// console.log(suma);      // 8
+// console.log(producto);  // 8
+// console.log(division);  // 5
+
+
+// boolean
+
